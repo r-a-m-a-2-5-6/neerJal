e839823e1f0fc7ffff7b9ec6f42d0ab0ec755bc6
Rama Krishna <sivaramakrishnayelam8939@gmail.com>
2025-02-25 01:44:30 +0530

    testvedios modified

--- public/js/mapsapi.js
+++ public/js/mapsapi.js
@@ -65,4 +65,21 @@ const map = new mapboxgl.Map({
     document.getElementById(sectionId).style.display = show ? 'block' : 'none';}
 
 
+    (() => {
+      'use strict'
     
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      const forms = document.querySelectorAll('.needs-validation')
+    
+      // Loop over them and prevent submission
+      Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+    
+          form.classList.add('was-validated')
+        }, false)
+      })
+    })()  
